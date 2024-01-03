da3e6393bdb13f6c50d19cdb2e81a9b7f2731a4d
Ashansa Mahra <mahraashansa1997@gmail.com>
2024-01-03 19:11:44 +0530

    Add files via upload

---
+++ Simple_Array_Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿class Result
+{
+    public static int SimpleArraySum(List<int> ar)
+    {
+        int sum = 0;
+        foreach (var e in ar)
+        {
+            sum += e;
+        }
+        return sum;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.SimpleArraySum(ar);
+
+        Console.WriteLine(result);
+    }
+}
